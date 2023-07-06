9fdd899e143105a246cd12e91503e751844f0742
Adam Guthrie <adma.c.guthrie@diamondgradelabs.com>
2023-07-06 16:50:10 -0500

    Fixed Bug Invalid DOM bug in quote Removed old butter knife template files Added basic css document

--- app.c
+++ app.c
@@ -6,7 +6,7 @@ void quote()
 {
     REQUEST *req = CREATE_REQUEST("https://ron-swanson-quotes.herokuapp.com/v2/quotes","",0);
     RESPONSE *res = GET(req);
-    SET_INNER_HTML_BY_ID("quoteResonse", res->body);
+    SET_INNER_HTML_BY_ID("quoteResponse", res->body);
     FREE_REQUEST(req);
     FREE_RESPONSE(res);
 }
